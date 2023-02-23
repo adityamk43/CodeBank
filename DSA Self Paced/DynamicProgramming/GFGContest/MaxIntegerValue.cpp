8fd254965be4783195ca31c3bbd6ca6b23f3ba94
adityamk43 <adityamk43@gmail.com>
2023-02-23 23:04:43 +0530

    Anagrams and DynamicProgramming

--- DSA Self Paced/DynamicProgramming/GFGContest/MaxIntegerValue.cpp
+++ DSA Self Paced/DynamicProgramming/GFGContest/MaxIntegerValue.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 
-int maxIntVal(string value, int n)
+long long int maxIntVal(string value, int n)
 {
-    int res = value[0] - '0';
+    long long int res = value[0] - '0';
 
     for (int i = 1; i < n; i++)
     {
-        res = max(res, max(res + (value[i] - '0'), res * (value[i] - '0')));
+        res = max(res + (value[i] - '0'), res * (value[i] - '0'));
     }
 
     return res;
