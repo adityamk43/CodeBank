9bf62584b3461c4e7d1da28dd6617da9ccddff3d
adityamk43 <adityamk43@gmail.com>
2023-08-07 23:45:42 +0530

    Neeraj Walia (EzSnippet) DSA Challenge DAY - 9

--- EzSnippet(Neeraj Walia) - DSA/EncodeAndDecodeStrings.ts
+++ EzSnippet(Neeraj Walia) - DSA/EncodeAndDecodeStrings.ts
@@ -1,4 +1,5 @@
 /**
+ * @event Neeraj Walia (EzSnippet) DSA Challenge DAY - 9
  * @description Design an algorithm to encode a list of strings to a string. The encoded string is sent over the network and is decoded back to the original list of strings.
  *
  * Please implement encode and decode.
