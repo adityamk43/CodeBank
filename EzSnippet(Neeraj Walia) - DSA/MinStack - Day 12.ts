cfd62da15ea83e984a6e5bc44a6d33f047a1f1d8
adityamk43 <adityamk43@gmail.com>
2023-08-11 00:45:28 +0530

    Min Stack Day -12

--- EzSnippet(Neeraj Walia) - DSA/MinStack - Day 12.ts
+++ EzSnippet(Neeraj Walia) - DSA/MinStack - Day 12.ts
@@ -54,40 +54,99 @@
     52.8%
  */
 
+/**
+ * MY IMPLEMENTATION
+ * @description
+ *  Runtime
+    Details
+    91ms
+    Beats 94.50%of users with TypeScript
+
+    Memory
+    Details
+    51.67mb
+    Beats 52.93%of users with TypeScript
+ */
+// class MinStack {
+//   private stack: number[];
+//   private minStack: number[];
+
+//   constructor() {
+//     this.stack = [];
+//     this.minStack = [];
+//   }
+
+//   push(val: number): void {
+//     this.stack.push(val);
+
+//     if (this.minStack.length !== 0)
+//       this.minStack.push(
+//         Math.min(val, this.minStack[this.minStack.length - 1])
+//       );
+//     else this.minStack.push(val);
+//   }
+
+//   pop(): void {
+//     if (this.stack.length !== 0) {
+//       this.stack.pop();
+//     }
+//     if (this.minStack.length !== 0) {
+//       this.minStack.pop();
+//     }
+//   }
+
+//   top(): number {
+//     return this.stack[this.stack.length - 1];
+//   }
+
+//   getMin(): number {
+//     return this.minStack[this.minStack.length - 1];
+//   }
+// }
+
+/**
+ * @description Another efficient implementation using only this stack having element as objects with two properties:- value pushed and minimum value
+ * 
+ *  Runtime
+    Details
+    93ms
+    Beats 92.59%of users with TypeScript
+
+    Memory
+    Details
+    50.78mb
+    Beats 90.68%of users with TypeScript
+
+
+ *  NOTE: There is not much difference between my implementation and this implementation both are same in speed but have different approaches!! and both are best according to me!
+ */
 class MinStack {
-  private stack: number[];
-  private minStack: number[];
+  private stack: { value: number; minVal: number }[];
 
   constructor() {
     this.stack = [];
-    this.minStack = [];
   }
 
   push(val: number): void {
-    this.stack.push(val);
-
-    if (this.minStack.length !== 0)
-      this.minStack.push(
-        Math.min(val, this.minStack[this.minStack.length - 1])
-      );
-    else this.minStack.push(val);
+    this.stack.push({
+      value: val,
+      minVal:
+        this.stack.length === 0
+          ? val
+          : Math.min(val, this.stack[this.stack.length - 1].minVal),
+    });
   }
 
   pop(): void {
-    if (this.stack.length !== 0) {
-      this.stack.pop();
-    }
-    if (this.minStack.length !== 0) {
-      this.minStack.pop();
-    }
+    this.stack.pop();
   }
 
   top(): number {
-    return this.stack[this.stack.length - 1];
+    return this.stack[this.stack.length - 1].value;
   }
 
   getMin(): number {
-    return this.minStack[this.minStack.length - 1];
+    return this.stack[this.stack.length - 1].minVal;
   }
 }
 
@@ -99,3 +158,39 @@ class MinStack {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+/**
+ * I/P:
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ */
+let val: number;
+
+// 1. MinStack
+var obj = new MinStack();
+
+// 2. push
+val = -2;
+obj.push(val);
+
+// 3. push
+val = 0;
+obj.push(val);
+
+// 4. push
+val = -3;
+obj.push(val);
+
+// 5. getMin
+console.log(obj.getMin()); //return -3
+
+// 6. pop
+obj.pop();
+
+// 7. top
+var param_3 = obj.top(); //return 0
+console.log(param_3);
+
+// 8. getMin
+var param_4 = obj.getMin(); //return -2
+console.log(param_4);
